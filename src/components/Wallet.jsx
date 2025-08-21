963e1a6c0f88a2d4ec263943355d01bd109e40be
kashifrind143-dev <kashifrind143@gmail.com>
2025-08-21 05:59:33 +0500

    Create Wallet.jsx

---
+++ src/components/Wallet.jsx
@@ -0,0 +1,7 @@
+export default function Wallet({ points }) {
+  return (
+    <div className="fixed bottom-4 right-4 px-4 py-2 bg-gray-800 rounded-xl shadow-lg">
+      🦁 Wallet: {points} ROAR
+    </div>
+  );
+}
